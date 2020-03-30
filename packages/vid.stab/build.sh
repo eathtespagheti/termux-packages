db7047d393711a5cfae6287373dcac764352218d
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-30 02:01:13 +0200

    ADD test build.sh

---
+++ packages/vid.stab/build.sh
@@ -0,0 +1,17 @@
+# http(s) link to package home page.
+TERMUX_PKG_HOMEPAGE=https://github.com/georgmartius/vid.stab
+
+# One-line, short package description.
+TERMUX_PKG_DESCRIPTION="Video stabilization library"
+
+# License.
+TERMUX_PKG_LICENSE="GPL-2.0"
+
+# Version.
+TERMUX_PKG_VERSION=1.1.0
+
+# URL to archive with source code.
+TERMUX_PKG_SRCURL=https://github.com/georgmartius/vid.stab/archive/v${TERMUX_PKG_VERSION}.tar.gz
+
+# SHA-256 checksum of the source code archive.
+TERMUX_PKG_SHA256=14d2a053e56edad4f397be0cb3ef8eb1ec3150404ce99a426c4eb641861dc0bb
